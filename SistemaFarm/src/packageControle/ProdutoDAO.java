10b36726b7878e14b58e8d8518d32cba4e92037b
vsistema <vsistema@LAB5-10.sistemafieto.rede>
2024-10-17 15:20:57 -0300

    Algumas alterações

--- SistemaFarm/src/packageControle/ProdutoDAO.java
+++ SistemaFarm/src/packageControle/ProdutoDAO.java
@@ -281,10 +281,24 @@ public class ProdutoDAO {
 			{
 				Produto p = new Produto();
 				p.setIdProduto(rs.getString(1));
-				p.setEndereco(rs.getString(2));
+				p.setIdFornecedor(rs.getString(2));
 				p.setNomeComecial(rs.getString(3));
-				p.setEstoque(rs.getString(4));
-
+				p.setNomeGenerico(rs.getString(4));
+				p.setCategoria(rs.getString(5));
+				p.setFormaFarmaceutica(rs.getString(6));
+				p.setConcentracao(rs.getString(7));
+				p.setDosagem(rs.getString(8));
+				p.setCodigo(rs.getString(9));
+				p.setEstoque(rs.getString(10));
+				p.setPreocoUN(rs.getString(11));
+				p.setTipoUN(rs.getString(12));
+				p.setDataFab(rs.getString(13));
+				p.setDataVal(rs.getString(14));
+				p.setRegistroAnvisa(rs.getString(15));
+				p.setLote(rs.getString(16));
+				p.setEndereco(rs.getString(17));
+				p.setPrincAtivo(rs.getString(18));
+				
 				produto.add(p);
 			}
 			
