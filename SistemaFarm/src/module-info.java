d24f0b6594de0aff685852893cef9174f904a6f4
Arthursaliz <arthurrodriguessantoslima@gmail.com>
2024-10-17 17:10:47 -0300

    METODO IMPRIMIR

--- SistemaFarm/src/module-info.java
+++ SistemaFarm/src/module-info.java
@@ -4,13 +4,15 @@ module Sistema_BluePen {
 	requires javafx.fxml;
 	requires javafx.base;
 	requires javafx.graphics;
+	requires org.apache.pdfbox;
+	requires org.apache.commons.logging;
+	//requires pdfbox.app;
 	
 	exports packageController;
 	exports packageModel;
 	exports packageControle;
 
-
-	opens packageController to javafx.fxml;
+	opens packageController to javafx.fxml, pdfbox.app, commons.logging ;
 	opens packageModel to javafx.fxml;
 	opens packageControle to javafx.fxml;
 
